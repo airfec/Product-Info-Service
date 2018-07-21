c69453070320ecf6a52ccc761064590a0d86edf8
OzgeYurtsever <ozge.yurtsever80@gmail.com>
2018-07-20 19:40:59 -0700

    Get data from dB to client succesfully
    - Implements router, contoller, and client ajax call to
    console log data

--- client/src/components/Applet/Applet.js
+++ client/src/components/Applet/Applet.js
@@ -1,22 +1,36 @@
-import React, { Component } from 'react';
-import FontAwesome from 'react-fontawesome';
+import React, { Component } from "react";
+import FontAwesome from "react-fontawesome";
+import $ from "jquery";
 
 class Applet extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      hello: 'world'
+      hello: "world"
     };
   }
 
   // componentWillMount(){}
-  // componentDidMount(){}
+  componentDidMount() {
+    const id = parseInt(window.location.pathname.split("/").pop());
+    const self = this;
+    $.get(`/api/rooms/${id}`, function(data) {
+      console.log("success got data", data);
+      // self.setState({ hello: data });
+    }).fail(function() {
+      alert("error");
+    });
+  }
   // componentWillUnmount(){}
 
   // componentWillReceiveProps(){}
   // shouldComponentUpdate(){}
   // componentWillUpdate(){}
   // componentDidUpdate(){}
+
+  // getRoomInfo() {
+
+  // }
 
   render() {
     return (
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-const express = require('express');
+const app = require("express");
+const ctrl = require("./../controllers");
+const router = app.Router();
+const bodyParser = require("body-parser");
 
-const ctrl = require('./../controllers');
-
-const router = express.Router();
-
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 // router.get('/', ctrl.main.home);
+
+router.get("/rooms/:id", (req, res) => {
+  console.log("here", req.params.id);
+  ctrl.getRoom(req.params.id, (err, data) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.json(data);
+    }
+  });
+});
 
 module.exports = router;
