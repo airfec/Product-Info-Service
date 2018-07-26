07b12d05bb579f4193745419d312fce8034179ca
OzgeYurtsever <ozge.yurtsever80@gmail.com>
2018-07-26 12:09:03 -0700

    Implement pop-up using React-modal

---
+++ client/src/components/Amenities/Icons.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesome';
+
+const icons = props => {
+  <div> </div>;
+};
+export default Icons;
--- client/src/components/Amenities/PopUp.js
+++ client/src/components/Amenities/PopUp.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import FontAwesome from 'react-fontawesome';
+import ReactModal from 'react-modal';
+ReactModal.setAppElement('#root');
 
 class PopUp extends Component {
   constructor(props) {
