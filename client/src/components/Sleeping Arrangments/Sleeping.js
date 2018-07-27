06a17a0a8a39564cdbcfe8623bec4ec95850aa99
OzgeYurtsever <ozge.yurtsever80@gmail.com>
2018-07-27 11:04:03 -0700

    Implement arrow display logic

--- client/src/components/Sleeping Arrangments/Sleeping.js
+++ client/src/components/Sleeping Arrangments/Sleeping.js
@@ -4,29 +4,45 @@ import Tile from './Tile.js';
 class Sleeping extends Component {
   constructor(props) {
     super(props);
+    console.log('props', this.props.room);
     this.state = {
       start: 0,
       end: 2,
-      showRightArrow: true,
-      showLeftArrow: true
+      showRightArrow: false,
+      showLeftArrow: false
     };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.populateArrows();
+    // console.log(this.state.start);
+    // console.log(this.state.end);
+  }
 
   populateTiles() {
-    // if (this.props.room.sleeping_arrangements.length)
+    // let arrangementArr = this.props.room.sleeping_arrangements || [];
+    // console.log('aarr', arrangementArr);
+    // arrangementArr.length > 3
+    //   ? this.setState({ start: 0, end: 2 })
+    //   : this.setState({ start: 0, end: arrangementArr.length - 1 });
+    // // console.log(this.state.start);
+    // // console.log(this.state.end);
   }
 
   populateArrows() {
     let arrangementArr = this.props.room.sleeping_arrangements || [];
-    this.state.start > 0
-      ? this.setState({ showLeftArrow: true })
-      : this.setState({ showLeftArrow: false });
-    this.state.end < arrangementArr.length
-      ? this.setState({ showRightArrow: true })
-      : this.setState({ showRightArrow: false });
+
+    arrangementArr.length > 3
+      ? this.setState({ showLeftArrow: false, showRightArrow: true })
+      : this.setState({ showLeftArrow: false, showRightArrow: false });
+    // this.state.end < arrangementArr.length
+    //   ? this.setState({ showRightArrow: true })
+    //   : this.setState({ showRightArrow: false });
   }
+
+  handleClickRightArrow() {}
+
+  handleClickLeftArrow() {}
 
   render() {
     let allTiles = this.props.room.sleeping_arrangements || [];
