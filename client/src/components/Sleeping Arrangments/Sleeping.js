da2dcccec2b54cbd1fb88baa0af21b32e3f7d623
OzgeYurtsever <ozge.yurtsever80@gmail.com>
2018-07-27 09:01:48 -0700

    Display tiles side by side

--- client/src/components/Sleeping Arrangments/Sleeping.js
+++ client/src/components/Sleeping Arrangments/Sleeping.js
@@ -4,16 +4,43 @@ import Tile from './Tile.js';
 class Sleeping extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      start: 0,
+      end: 2,
+      showRightArrow: false,
+      showLeftArrow: false
+    };
   }
 
-  populateTiles() {}
+  componentDidMount() {}
+
+  populateTiles() {
+    // if (this.props.room.sleeping_arrangements.length)
+  }
+
+  populateArrows() {
+    // this.state.start > 0
+    //   ? this.setState({ showLeftArrow: true })
+    //   : this.setState({ showLeftArrow: false });
+    // this.state.end < this.props.room.sleeping_arrangements.length
+    //   ? this.setState({ showRightArrow: true })
+    //   : this.setState({ showRightArrow: false });
+  }
 
   render() {
+    let allTiles = this.props.room.sleeping_arrangements || [];
+    let displayTiles = allTiles.slice(this.state.start, this.state.end + 1);
+    // this.populateArrows();
+
     return (
       <div className="sleeping">
         <div className="sleeping__title">Sleeping arrangements</div>
-        <Tile bedroomNo={3} detail={'2 queen beds'} />
+        <Tile
+          detail={displayTiles}
+          rightArrow={this.state.showRightArrow}
+          leftArrow={this.state.showLeftArrow}
+        />
+
         <div className="line"> </div>
       </div>
     );
--- client/src/components/Sleeping Arrangments/Tile.js
+++ client/src/components/Sleeping Arrangments/Tile.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 import FontAwesome from 'react-fontawesome';
 
-const Tile = ({ bedroomNo, detail }) => {
-  const imgArr = [];
-  const imgCount = parseInt(detail[0]);
-  for (let i = 0; i < imgCount; i++) {
-    let bedImg = (
-      <span className="tile-image icons">
-        <FontAwesome name="bed" size="lg" />
-      </span>
-    );
-    imgArr.push(bedImg);
-  }
-  return (
-    <div className="tile">
-      <div className="tile__body" align="center">
-        <div className="images">{imgArr}</div>
-        <div>Bedroom {bedroomNo}</div>
-        <div className="bed-detail">{detail}</div>
+const Tile = props => {
+  let tiles = [];
+  // if ()
+  for (let j = 0; j < props.detail.length; j++) {
+    let imgArr = [];
+    const imgCount = parseInt(props.detail[j][0]);
+    for (let i = 0; i < imgCount; i++) {
+      let bedImg = (
+        <span className="tile-image icons">
+          <FontAwesome name="bed" size="lg" />
+        </span>
+      );
+      imgArr.push(bedImg);
+    }
+    let tile = (
+      <div className="tile">
+        <div className="tile__body" align="center">
+          <div className="images">{imgArr}</div>
+          <div>Bedroom {j + 1}</div>
+          <div className="bed-detail">{props.detail[j]}</div>
+        </div>
       </div>
-    </div>
-  );
+    );
+    tiles.push(tile);
+  }
+  return <div className="tile--display">{tiles}</div>;
 };
 export default Tile;
