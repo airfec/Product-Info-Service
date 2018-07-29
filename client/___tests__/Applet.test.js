5b4182148d3f5ef5ec93325c5712d488a847b78e
OzgeYurtsever <ozge.yurtsever80@gmail.com>
2018-07-28 18:41:57 -0700

    Add configuration to package.json

--- client/___tests__/Applet.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Applet from '../src/components/Applet/Applet.js';
-
-describe('Applet', () => {
- it('should be defined', () => {
-   expect(Applet).toBeDefined();
- });
-});
