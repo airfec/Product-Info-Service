1d125e485ee4a795367a72bf46a2cfcbca196d98
OzgeYurtsever <ozge.yurtsever80@gmail.com>
2018-07-31 11:25:22 -0700

    Change capitalization of Rules

--- client/src/components/Rules/Rules.js
+++ client/src/components/Rules/Rules.js
@@ -14,15 +14,22 @@ class Rules extends Component {
     this.setState({ isMoreClicked: !this.state.isMoreClicked });
   }
 
+  capitilalize(string) {
+    let first = string[0].toUpperCase();
+    let rest = string.substring(1);
+    return first + rest;
+  }
+
   render() {
     let rulesArr = this.props.room.house_rules;
 
     if (typeof rulesArr !== 'object') {
       rulesArr = ['here', 'test test'];
     }
+
     let rules = rulesArr.map((rule, index) => (
       <div key={index} className="list__item">
-        {rule}
+        {this.capitilalize(rule)}
       </div>
     ));
 
@@ -32,6 +39,7 @@ class Rules extends Component {
         <div className="rules__list">{rules}</div>
         {this.state.isMoreClicked ? (
           <div>
+            <div className="rules-toggle__border--on" />
             <div className="rules--explanation">
               {this.props.room.house_rules_description}
             </div>
