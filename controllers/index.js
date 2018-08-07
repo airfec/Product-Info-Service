57db886ad06a7ea1acf608d0c65b88afae24f70e
SamMahani <sam.amirmahani@gmail.com>
2018-08-07 16:10:47 -0700

    Implement delete functionality

--- controllers/index.js
+++ controllers/index.js
@@ -13,4 +13,37 @@ const getRoom = (roomId, callback) => {
   });
 };
 
-module.exports = { getRoom };
+const postRoom = (roomId, callback) => {
+  let query = db.Room.findOne({ room_id: roomId });
+  query.exec((err, room) => {
+    if (err) {
+      console.log("error in getting room: ", err);
+      callback(err);
+    }
+    callback(null, room);
+  });
+};
+
+const updateRoom = (roomId, callback) => {
+  let query = db.Room.findOne({ room_id: roomId });
+  query.exec((err, room) => {
+    if (err) {
+      console.log("error in getting room: ", err);
+      callback(err);
+    }
+    callback(null, room);
+  });
+};
+
+const deleteRoom = (roomId, callback) => {
+  let query = db.Room.deleteOne({ room_id: roomId });
+  query.exec((err, room) => {
+    if (err) {
+      console.log("error in getting room: ", err);
+      callback(err);
+    }
+    callback(null, room);
+  });
+};
+
+module.exports = { getRoom, postRoom, updateRoom, deleteRoom};
--- routes/index.js
+++ routes/index.js
@@ -9,36 +9,52 @@ const jsonParser = bodyParser.json();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 // router.get('/', ctrl.main.home);
 
-const callback = (err, data) => {
+router.get('/rooms/:id', (req, res) => {
+  console.log('here inside routes/index', req.params.id);
+  ctrl.getRoom(req.params.id, (err, data) => {
     if (err) {
       res.status(404).send(err);
     } else {
       res.json(data);
     }
-};
-
-router.get('/rooms/:id', (req, res) => {
-  console.log('here inside routes/index', req.params.id);
-  ctrl.getRoom(req.params.id, callback)
+  });
 });
 
 
 // CREATE
 router.post('/rooms/:id', (req, res) => {
   console.log('POST-- here inside routes/index', req.params.id);
-  ctrl.postRoom(req.params.id, callback)
+  ctrl.postRoom(req.params.id, (err, data) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.json(data);
+    }
+  });
 });
 
 // UPDATE
 router.put('/rooms/:id', (req, res) => {
   console.log('PUT-- here inside routes/index', req.params.id);
-  ctrl.getRoom(req.params.id, callback)
+  ctrl.updateRoom(req.params.id, (err, data) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.json(data);
+    }
+  });
 });
 
 // DELETE
 router.delete('/rooms/:id', (req, res) => {
   console.log('DELETE-- here inside routes/index', req.params.id);
-  ctrl.getRoom(req.params.id, callback)
+  ctrl.deleteRoom(req.params.id, (err, data) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.json(data);
+    }
+  });
 });
 
 module.exports = router;
