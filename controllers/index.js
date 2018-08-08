07615e70178cd4ce709a82f9d2238eb29861ecfd
Ricardo <rroque98@gmail.com>
2018-08-07 20:40:37 -0700

    Add API support for post request

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,19 @@
-/* 
-func (req, res) {
-
-}
-*/
 const db = require("../models");
+const Room = require("../models/Room.js");
+// const db = require("../models");
 
+// Create:
+const postRoomInfo = (data, callback) => {
+  Room.create(data, (err, result) => {
+    if(err) {
+      callback(err);
+    } else {
+      callback(null, result);
+    }
+  });
+};
+
+// Read:
 const getRoom = (roomId, callback) => {
   let query = db.Room.findOne({ room_id: roomId });
   query.exec((err, room) => {
@@ -17,3 +26,34 @@ const getRoom = (roomId, callback) => {
 };
 
 module.exports = { getRoom };
+
+// Update:
+const updateRoomInfo = (roomId, callback) => {
+  // let query = db.Room.findOne({ room_id: roomId });
+  // query.exec((err, room) => {
+  //   if (err) {
+  //     console.log("error in getting room: ", err);
+  //     callback(err);
+  //   }
+  //   callback(null, room);
+  // });
+};
+
+//Delete:
+const deleteRoomInfo = (roomId, callback) => {
+  // let query = db.Room.findOne({ room_id: roomId });
+  // query.exec((err, room) => {
+  //   if (err) {
+  //     console.log("error in getting room: ", err);
+  //     callback(err);
+  //   }
+  //   callback(null, room);
+  // });
+};
+
+module.exports = {
+  getRoom,
+  postRoomInfo,
+  updateRoomInfo,
+  deleteRoomInfo,
+};
